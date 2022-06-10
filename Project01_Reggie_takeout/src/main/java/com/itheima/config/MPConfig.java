a408dc6174e655aaecb94a3759448dd8504815af
szughx <szughx@outlook.com>
2022-06-10 08:45:16 +0800

    MyBatis分页拦截器配置类

---
+++ Project01_Reggie_takeout/src/main/java/com/itheima/config/MPConfig.java
@@ -0,0 +1,23 @@
+package com.itheima.config;
+
+/*
+    用来实现MyBatisPlus的分页插件
+ */
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MPConfig {
+
+    //配置MyBatis分页拦截器
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+
+        return interceptor;
+    }
+}
